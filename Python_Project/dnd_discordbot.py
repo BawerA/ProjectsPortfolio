f49572b21966a04594351548f13db9dbeafa7088
BawerA <51058978+BawerA@users.noreply.github.com>
2021-04-08 16:31:38 -0400

    Update dnd_discordbot.py

--- Python_Project/dnd_discordbot.py
+++ Python_Project/dnd_discordbot.py
@@ -21,11 +21,46 @@ async def on_member_remove(member): #When someone leaves the server
 @client.command() #Filler to add later for DND BOT, will send all commands to user
 async def helpme(ctx):
     await ctx.send('Hey there! I am a Dungeons and Dragons Bot that will help you create a character!')
+    
+
+#Dice rolls are below
+
+@client.command() #Test for rolling function of the bot, currently using D4
+async def roll2(ctx):
+    dice_roll2 = ['1','2']
+    await ctx.send('You rolled a {}!'.format(random.choice(dice_roll2)))
 
 @client.command() #Test for rolling function of the bot, currently using D4
 async def roll4(ctx):
     dice = ['1','2','3','4']
     await ctx.send('You rolled a {}!'.format(random.choice(dice)))
+    
+@client.command() #Test for rolling function of the bot, currently using D4
+async def roll6(ctx):
+    dice1 = ['1','2','3','4','6']
+    await ctx.send('You rolled a {}!'.format(random.choice(dice1)))
+
+@client.command() #Test for rolling function of the bot, currently using D4
+async def roll8(ctx):
+    dice2 = ['1','2','3','4','6','7','8']
+    await ctx.send('You rolled a {}!'.format(random.choice(dice2)))
+    
+@client.command() #Test for rolling function of the bot, currently using D4
+async def roll10(ctx):
+    dice3 = ['1','2','3','4','6','7','8', '9','10']
+    await ctx.send('You rolled a {}!'.format(random.choice(dice3)))
+    
+@client.command() #Test for rolling function of the bot, currently using D4
+async def roll12(ctx):
+    dice4 = ['1','2','3','4','6','7','8', '9','10','11','12']
+    await ctx.send('You rolled a {}!'.format(random.choice(dice4)))
+    
+@client.command() #Test for rolling function of the bot, currently using D4
+async def roll20(ctx):
+    dice5 = ['1','2','3','4','6','7','8', '9','10','11','12','13','14','15','16','17','18','19','20']
+    await ctx.send('You rolled a {}!'.format(random.choice(dice5)))
+
+#Below are the rolls for races and classes
     
 @client.command() #Test for rolling function of the bot, currently using D4
 async def race(ctx):
@@ -36,6 +71,7 @@ async def race(ctx):
 async def classes(ctx):
     classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin','Ranger','Rogue', 'Sorcerer', 'Warlock', 'Wizard', 'Artificer', 'Blood Hunter']
     await ctx.send('Your class is {}!'.format(random.choice(classes)))
+   
 client.run('') #Links the code to bot app
 
 
